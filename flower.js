32c9b43a2069bdb8b624d3f1c3cf795ed0278081
Angel-Marc <abingabing26@gmail.com>
2025-04-07 16:00:53 +0800

    Add files via upload

---
+++ flower.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+  const audio = document.getElementById('background-audio');
+  
+  // Add a click event listener to play audio on user interaction
+  document.body.addEventListener('click', () => {
+    audio.play();
+  });
+  // Hide the welcome sequence after the animation finishes
+  setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    const seq = document.getElementById("welcome-sequence");
+    if (seq) seq.style.display = "none"; // Hide the welcome sequence after it's done
+  }, 8000); // Adjust the timeout to match the total duration of your animation
+};
